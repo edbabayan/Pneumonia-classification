2f0eb575392d3fb61a81561dc1ed7ce5c18897b8
eduard <eduard.babayan@cognaize.com>
2024-06-17 20:28:55 +0400

    CHANGED the max_epoch=50, monitor=f1_score, save_k=5

--- preprocess/preprocessing.py
+++ preprocess/preprocessing.py
@@ -10,33 +10,35 @@ from preprocess.preprocess_config import CFG
 class Preprocessor:
     def __init__(self, config):
         self.config = config
-        self.labels = pd.read_csv(self.config.train_labels_csv).drop_duplicates(subset='patientId')
+        self.labels = pd.read_csv(self.config.train_labels_csv).drop_duplicates("patientId")
         self.sums = 0
         self.sums_squared = 0
 
     def process(self):
-        for c, patient in enumerate(tqdm(self.labels['patientId'], desc='Processing images')):
-            patient_id = self.labels.patientId.iloc[c]
-            dcm_path = self.config.train_images.joinpath(patient_id).with_suffix('.dcm')
+        for c, patient_id in enumerate(tqdm(self.labels.patientId)):
+            dcm_path = CFG.train_images / patient_id
+            dcm_path = dcm_path.with_suffix(".dcm")
+
             dcm = pydicom.read_file(dcm_path).pixel_array / 255
 
-            dcm_array = cv2.resize(dcm, (self.config.image_size, self.config.image_size)).astype(np.float16)
+            dcm_array = cv2.resize(dcm, (224, 224)).astype(np.float16)
 
             label = self.labels.Target.iloc[c]
-            train_or_valid = 'train' if c < 0.8 * len(self.labels) else 'valid'
 
-            current_save_path = self.config.output_path.joinpath(train_or_valid).joinpath(str(label))
+            train_or_val = "train" if c < 24000 else "val"
+
+            current_save_path = CFG.output_path / train_or_val / str(label)
             current_save_path.mkdir(parents=True, exist_ok=True)
-            np.save(current_save_path.joinpath(patient_id), dcm_array)
+            np.save(current_save_path / patient_id, dcm_array)
 
-            normalizer = 224 * 224
-            if train_or_valid == 'train':
+            normalizer = dcm_array.shape[0] * dcm_array.shape[1]
+            if train_or_val == "train":
                 self.sums += np.sum(dcm_array) / normalizer
-                self.sums_squared += np.sum(dcm_array ** 2) / normalizer
+                self.sums_squared += (np.power(dcm_array, 2).sum()) / normalizer
 
     def get_statistics(self):
-        mean = self.sums / len(self.labels) * 0.8
-        std = np.sqrt(self.sums_squared / len(self.labels) * 0.8 - mean ** 2)
+        mean = self.sums / 24000
+        std = np.sqrt(self.sums_squared / 24000 - (mean ** 2))
         return mean, std
 
 
