804aa44c0385f927b96401bfcc50446c9254ee9c
eduard <eduard.babayan@cognaize.com>
2024-06-17 20:32:11 +0400

    restructured the CFG class

--- model/config.py
+++ model/config.py
@@ -3,6 +3,10 @@ import torch
 
 
 class CFG:
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    image_size = 256
+
     root = Path(__file__).parent.parent.absolute()
     data = root.joinpath('data')
     checkpoints = root.joinpath('checkpoints')
